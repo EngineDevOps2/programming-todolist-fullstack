28e05a55434796b5f5626d90d26693ae1f28c304
EngineDevOps <enginedevops@gmail.com>
2024-12-17 13:15:56 -0500

    Update api.php

--- todo-list-backend-laravel/todolist/routes/api.php
+++ todo-list-backend-laravel/todolist/routes/api.php
@@ -1,8 +1,10 @@
 <?php
-namespace App\Http\Controllers;  // Ensure the namespace matches the directory structure  
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;  
 
-use Illuminate\Http\Request;  
-use App\Models\Todo; // Adjust as necessary  
-
-class TodoController extends Controller  
-Route::apiResource('todos', TodoController::class);
+Route::get('/todos', [TodoController::class, 'index']);  
+Route::post('/todos', [TodoController::class, 'store']);  
+Route::get('/todos/{id}', [TodoController::class, 'show']);  
+Route::put('/todos/{id}', [TodoController::class, 'update']);  
+Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
