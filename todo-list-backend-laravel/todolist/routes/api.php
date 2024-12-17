424273ec998b39b59caad4854f05aa6abf46aa81
EngineDevOps <enginedevops@gmail.com>
2024-12-17 13:08:34 -0500

    Update api.php

--- todo-list-backend-laravel/todolist/routes/api.php
+++ todo-list-backend-laravel/todolist/routes/api.php
@@ -1,13 +1,6 @@
-// <?php
-// use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\TodoController;  
-
-// Route::get('/api/todos', [TodoController::class, 'index']);  
-// Route::post('/api/todos', [TodoController::class, 'store']);  
-// Route::get('/api/todos/{id}', [TodoController::class, 'show']);  
-// Route::put('/api/todos/{id}', [TodoController::class, 'update']);  
-// Route::delete('/api/todos/{id}', [TodoController::class, 'destroy']);
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 use AppHttpControllersTodoController;
 
