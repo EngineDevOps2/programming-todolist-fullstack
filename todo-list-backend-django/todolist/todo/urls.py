a61601704402c88a5a8af99fb6185ac258337503
EngineDevOps <enginedevops@gmail.com>
2024-12-18 05:25:07 -0500

    Update urls.py

--- todo-list-backend-django/todolist/todo/urls.py
+++ todo-list-backend-django/todolist/todo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from .views import TodoViewSet  
 from rest_framework.routers import SimpleRouter  
 router = SimpleRouter(trailing_slash=False)
-router = SimpleRouter()  # Use SimpleRouter to avoid trailing slashes  
+
 router.register(r'todos', TodoViewSet, basename='todo')  
 
 urlpatterns = router.urls  # This will generate URLs without trailing slashes
