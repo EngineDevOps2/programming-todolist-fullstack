16483e256b94c2cc6640c48c5b1d11c116aae222
EngineDevOps <enginedevops@gmail.com>
2024-12-25 08:30:42 -0500

    Update app.php

--- todo-list-backend-laravel/todolist/bootstrap/app.php
+++ todo-list-backend-laravel/todolist/bootstrap/app.php
@@ -18,12 +18,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'status' => 'false',
-                    'message' => 'Route Not found',
-                ], 404);
-            }
-        });
+//        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            // if ($request->is('api/*')) {
+            //     return response()->json([
+            //         'status' => 'false',
+            //         'message' => 'Route Not found',
+            //     ], 404);
+            // }
+    //    });
     })->create();
